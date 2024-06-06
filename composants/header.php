c3a9de821c912bdeb8271cd9c7c8c464d56c58af
Lucy Feneyrol <lucyfeneyrol@gmail.com>
2024-06-07 01:08:00 +0200

    Ajout responsive

--- composants/header.php
+++ composants/header.php
@@ -1,29 +1,27 @@
 <nav class="navbar">
     <h1><a href="/index.php" title="ACCUEIL">POTAGERS PARTAGES</a></h1>
-    <ul id="lien-pages">
-        <li <?php echo (basename($_SERVER['PHP_SELF']) == 'index.php') ? 'class="current"' : ''; ?>><a href="/index.php">Accueil</a></li>
-        <li <?php echo (basename($_SERVER['PHP_SELF']) == 'jardins.php') ? 'class="current"' : ''; ?>><a href="/jardins.php">Jardins</a></li>
-        <li <?php echo (basename($_SERVER['PHP_SELF']) == 'contact.php') ? 'class="current"' : ''; ?>><a href="/contact.php">Contact</a></li>
-    </ul>
-    <ul id="lien-inscription">
-    <?php
-        session_start();
+    <div id="menu-items">
+        <ul id="lien-pages">
+            <li <?php echo (basename($_SERVER['PHP_SELF']) == 'index.php') ? 'class="current"' : ''; ?>><a href="/index.php">Accueil</a></li>
+            <li <?php echo (basename($_SERVER['PHP_SELF']) == 'jardins.php') ? 'class="current"' : ''; ?>><a href="/jardins.php">Jardins</a></li>
+            <li <?php echo (basename($_SERVER['PHP_SELF']) == 'contact.php') ? 'class="current"' : ''; ?>><a href="/contact.php">Contact</a></li>
+        </ul>
+        <ul id="lien-inscription">
+            <?php
+            session_start();
 
-        if (isset($_SESSION['user_id'])) {
-            echo $_SESSION['user_nom'];
-            echo $_SESSION['user_prnm'];
-            echo '<a href="/users/deconnexion.php">Déconnexion</a>';
-            echo '<a href="/users/compte.php">Mon compte</a>';
-
-        } else {
-
-            echo '<li' . (basename($_SERVER['PHP_SELF']) == 'formConnexion.php' ? ' class="current"' : '') . '><a href="/users/formConnexion.php">Connexion</a></li>';
-            echo '<li>|</li>';
-            echo '<li' . (basename($_SERVER['PHP_SELF']) == 'formInscription.php' ? ' class="current"' : '') . '><a href="/users/formInscription.php">Inscription</a></li>';
-
-            // echo '<a href="/users/formConnexion.php">Connexion</a>';
-            // echo '<a href="/users/formInscription.php">Inscription</a>';
-        }
-        ?>
-    </ul>
+            if (isset($_SESSION['user_id'])) {
+                echo $_SESSION['user_nom'];
+                echo $_SESSION['user_prnm'];
+                echo '<a href="/users/deconnexion.php">Déconnexion</a>';
+                echo '<a href="/users/compte.php">Mon compte</a>';
+            } else {
+                echo '<li' . (basename($_SERVER['PHP_SELF']) == 'formConnexion.php' ? ' class="current"' : '') . '><a href="/users/formConnexion.php">Connexion</a></li>';
+                echo '<li>|</li>';
+                echo '<li' . (basename($_SERVER['PHP_SELF']) == 'formInscription.php' ? ' class="current"' : '') . '><a href="/users/formInscription.php">Inscription</a></li>';
+            }
+            ?>
+        </ul>
+    </div>
+    <button class="burger-menu">&#9776;</button>
 </nav>
--- index.php
+++ index.php
@@ -1,8 +1,7 @@
-
 <?php
-require 'model/connectBD.php';
-require('composants/head.php');
-require('composants/header.php');
+    //require('model/connectBD.php');
+    require('composants/head.php');
+    require('composants/header.php');
 ?>
 
     <div id="hero-image">
@@ -12,17 +11,45 @@ require('composants/header.php');
     </div>
 
     <div id="a-propos-container">
-        <h2>Qui nous sommes</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas ut rem animi nam enim quasi. Repellat, suscipit. Illo at ab laudantium sequi laborum quos numquam ex aliquam! Explicabo, quod itaque! Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, laboriosam iure consequatur maxime et exercitationem, minima placeat obcaecati esse, consequuntur dolores quia iste nesciunt eligendi quibusdam amet quam voluptatibus omnis! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis officia corporis at, dolorem vitae unde fuga aperiam minima beatae amet ipsa aliquid perspiciatis sint commodi sunt alias ut reiciendis molestiae.</p>
-        <div id="a-propos">
-            <div class="valeur"><img src="./src/assets/img/icon1.png"></div>
-            <div class="valeur"><img src="./src/assets/img/icon2.png"></div>
-            <div class="valeur"><img src="./src/assets/img/icon3.png"></div>
+        <h2>Notre concept</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas ut rem animi nam enim quasi. Repellat, suscipit. Illo at ab laudantium sequi laborum quos numquam ex aliquam! Explicabo, quod itaque! Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, laboriosam iure consequatur maxime et exercitationem, minima placeat obcaecati esse, consequuntur dolores quia iste nesciunt eligendi quibusdam amet quam voluptatibus omnis! Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>     
+    </div>
+
+    <div id="valeur-container">
+        <h2>Nos valeurs</h2>
+        <hr>
+        <div id="valeur">
+            <div class="valeur-fiche">
+                <img src="./src/assets/img/icon3.png">
+                <h3>Bien-être et santé</h3>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit exercitationem voluptas sunt fugit, tempore corporis, nemo fuga natus dolorem sint non rem dolore nihil. Dolor pariatur molestiae beatae illo ut.</p>
+            </div>
+            <div class="valeur-fiche">
+                <img src="./src/assets/img/icon2.png">
+                <h3>Lien social</h3>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit exercitationem voluptas sunt fugit, tempore corporis, nemo fuga natus dolorem sint non rem dolore nihil. Dolor pariatur molestiae beatae illo ut.</p>
+            </div>
+            <div class="valeur-fiche">
+                <img src="./src/assets/img/icon1.png">
+                <h3>Respect de l'environnement</h3>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit exercitationem voluptas sunt fugit, tempore corporis, nemo fuga natus dolorem sint non rem dolore nihil. Dolor pariatur molestiae beatae illo ut.</p>
+            </div>
+        </div>
+    </div>
+
+    <div id="galerie-container">
+        <h2>Nos potagers</h2>
+        <hr>
+        <div id="galerie">
+            <div class="galerie-item large"><img src="./src/assets/img/potager2.jpg" alt="Image 1"></div>
+            <div class="galerie-item small"><img src="./src/assets/img/potager1.jpg" alt="Image 2"></div>
+            <div class="galerie-item small"><img src="./src/assets/img/potager3.jpg" alt="Image 3"></div>
+            <div class="galerie-item large"><img src="./src/assets/img/potager4.jpg" alt="Image 4"></div>
         </div>
     </div>
 
     <div class="carousel-container">
-        <h2>Ce qu'ils en pensent</h2>
+        <h2>Quelques avis</h2>
         <hr>
         <button class="carousel-button left" onclick="moveLeft()">&#10094;</button>
         <div class="carousel">
@@ -70,7 +97,3 @@ require('composants/header.php');
     <?php require("./composants/footer.php") ?>
 
     <script src="./src/assets/js/script.js"></script>
-    
-<?php
-require('composants/footer.php');
-?>
