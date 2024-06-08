7b58b7f85a5620f7ed93e0eb7c51eec2e7dfa5f6
Elouan Bruzek <elouan.bruzek@etudiant.univ-reims.fr>
2024-06-08 15:52:13 +0200

    Update head.php

--- composants/head.php
+++ composants/head.php
@@ -8,9 +8,6 @@
     <title>Potagers Partagés - Accueil</title>
     <link href="/src/assets/css/style.css" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
-    <script type="module">
-        import { minidenticonSvg } from 'https://cdn.jsdelivr.net/npm/minidenticons@4.2.1/minidenticons.min.js'
-    </script>
 </head>
 
 <body>
