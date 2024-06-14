220079bb8952695330bff77701345ddc3e20d7f0
Elouan Bruzek <elouan.bruzek@etudiant.univ-reims.fr>
2024-06-14 17:39:02 +0200

    Modif header admin

--- composants/headerAdmin.php
+++ composants/headerAdmin.php
@@ -16,8 +16,8 @@
             <a href="#" id="mobile-menu-btn"><i class="fas fa-bars"></i></a>
             <nav id="mobile-menu">
                 <ul>
-                    <li <?php echo (basename($_SERVER['PHP_SELF']) == 'dashboard.php') ? 'class="current"' : ''; ?>><a href="../admin/dashboard.php">Dashboard</a></li>
-                    <li <?php echo (basename($_SERVER['PHP_SELF']) == 'tableParcelles.php') ? 'class="current"' : ''; ?>><a href="../admin/parcelles/tableParcelles.php">Les parcelles</a></li>
+                    <li <?php echo (basename($_SERVER['PHP_SELF']) == 'dashboard.php') ? 'class="current"' : ''; ?>><a href="/admin/dashboard.php">Dashboard</a></li>
+                    <li <?php echo (basename($_SERVER['PHP_SELF']) == 'tableParcelles.php') ? 'class="current"' : ''; ?>><a href="/admin/parcelles/tableParcelles.php">Les parcelles</a></li>
                     <li <?php echo (basename($_SERVER['PHP_SELF']) == 'tableJardins.php') ? 'class="current"' : ''; ?>><a href="/admin/jardins/tableJardins.php">Les jardins</a></li>
                     <li <?php echo (basename($_SERVER['PHP_SELF']) == 'tableUsers.php') ? 'class="current"' : ''; ?>><a href="/admin/users/tableUsers.php">Les utilisateurs</a></li>
                 </ul>
